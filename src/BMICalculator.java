0e37869a10ab7c396a03bcbad563cb48b213b710
Srao2020 <contact@surfsyde.com>
2022-11-14 10:49:30 -0500

    BMI Calc

---
+++ src/BMICalculator.java
@@ -0,0 +1,9 @@
+public class BMICalculator {
+    /** Convert English to metric units, perform the BMI calculation.
+     * NOTE: this method must properly handle bad data
+     * @param inches users height in inches
+     * @param pounds users weight in inches
+     * @return ysers BMI weight(kg)/height
+     */
+    public static double computeBMI(int inches, int pounds)
+}
